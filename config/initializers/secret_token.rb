ba5438a0b5253c35dc0f711e82c58c15f53b5b89
Imquantified <imquantified@gmail.com>
2014-02-24 13:05:27 -0600

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clanswers::Application.config.secret_key_base = 'a5925d500db257894a7428c2151da29774a0067a910fdd54ac539cc3c824d9c9930c174a66775147f47b7461be9e3bd35076ac02bbc4dabb6d676d0fb37ae267'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clanswers::Application.config.session_store :cookie_store, key: '_clanswers_session'
