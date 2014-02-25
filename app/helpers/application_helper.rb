99a283c452d91945a8ac7768fbefb7f1f5f68ea8
Imquantified <imquantified@gmail.com>
2014-02-24 18:50:47 -0600

    Create static pages and also controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def title_helper(page_title)
+		base_title = "Clanswers"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} - #{page_title}"
+		end
+	end
 end
