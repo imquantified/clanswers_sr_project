4fc0f3dcdccffc77bf0da1eb02ee663b27ecc832
Imquantified <imquantified@gmail.com>
2014-03-04 12:18:31 -0600

    Complete sign in and sign out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController\
+class SessionsController < ApplicationController
 	
 	def new
 	end
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController\
 	def create
 		user = User.find_by(username: params[:session][:username])
 		if user && user.authenticate(params[:session][:password])
-		 #
+			sign_in user
+			redirect_to user
 		else
 			flash.now[:danger] = "Invalid email/password combination"
 			render 'new'
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController\
 	end
 
 	def destroy
+		sign_out
+		redirect_to root_url
 	end
 end
