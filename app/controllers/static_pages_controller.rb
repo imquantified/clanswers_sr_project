304231dda6feaf69a3f8c731a20d029c283a0c66
Imquantified <imquantified@gmail.com>
2014-03-12 20:22:24 -0500

    Add styling to feeds and clean up interface

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
   def home
   	if signed_in?
 	  	@quick_question = current_user.quick_questions.build
-	  	@interest_question = current_user.interest_questions.build
-	  	@thoughtful_question = current_user.thoughtful_questions.build
+	  	# @interest_question = current_user.interest_questions.build
+	  	# @thoughtful_question = current_user.thoughtful_questions.build
   		#probably breaks
   		@feed_items = current_user.feed.paginate(page: params[:page])
   	end
