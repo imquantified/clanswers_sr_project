7b3628608d52e93f6219ab318c48a468c38400d7
Imquantified <imquantified@gmail.com>
2014-03-28 16:15:06 -0500

    Add expand feature to feed strings with css changes for sidebar and profile feed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,13 +11,13 @@ module ApplicationHelper
 
 
   def wrap(content)
-    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(" ")))
   end
 
   private
 
-    def wrap_long_string(text, max_width = 30)
-      zero_width_space = "&#8203;"
+    def wrap_long_string(text, max_width = 35)
+      zero_width_space = " "
       regex = /.{1,#{max_width}}/
       (text.length < max_width) ? text :
                                   text.scan(regex).join(zero_width_space)
